f1c3635bab24a6b569cb99bad050b2754ae0f5f3
leerobert807 <leerobert807@gmail.com>
2021-10-06 21:11:31 +0300

    Добавлена инкапсуляция класса Areas

--- python/hh_constants_api.py
+++ python/hh_constants_api.py
@@ -1,18 +1,27 @@
-r"""Модуль с константами API hh.ru"""
+"""Модуль с константами API hh.ru."""
 from enum import Enum
 
 url = 'https://api.hh.ru/vacancies'
 
+
 class Areas(Enum):
-  """Класс типа enum с кортежами, содержащими имена локации и номер в соответствии с API hh.ru"""
-  MOSCOW           = "Moscow"             , 1
-  SAINT_PETERSBURG = "Sankt-Peterburg"    , 2
-  VORONEZH         = "Voronezh"           , 26
-  RUSSIA           = "Russia"             , 113
-  MOSCOW_OBLAST    = "Moskovskaya oblast'", 2019
-  RED_MOUNTAIN     = "Krasnogorsk"        , 2034
+    """Класс типа enum с кортежами, содержащими имена локации и номер в соответствии с API hh.ru."""
 
-  def __init__(self, designation, key) -> None:
-    self.designation = designation
-    self.key         = key
+    MOSCOW = ('Moscow', 1)
+    SAINT_PETERSBURG = ('Sankt-Peterburg', 2)
+    VORONEZH = ('Voronezh', 26)
+    RUSSIA = ('Russia', 113)
+    MOSCOW_OBLAST = ('Moskovskaya oblast', 2019)
+    RED_MOUNTAIN = ('Krasnogorsk', 2034)
 
+    def __init__(self, _designation, _key) -> None:
+        self._designation = _designation
+        self._key = _key
+
+    @property
+    def designation(self) -> str:
+        return self._designation
+
+    @property
+    def key(self) -> int:
+        return self._key
