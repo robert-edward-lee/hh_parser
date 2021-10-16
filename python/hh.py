99d6b1cef3b87194e19d7b9f509687dd9ea053f6
leerobert807 <leerobert807@gmail.com>
2021-10-16 11:31:45 +0300

    Уточнение средней ЗП
    Теперь вместо среднего арифметического мы рассматриваем
    среднюю медианную ЗП в кач-ве среднего заработка

--- python/hh.py
+++ python/hh.py
@@ -2,6 +2,7 @@ import json
 import sys
 import time
 from datetime import datetime
+from statistics import median
 
 import pandas
 import requests
@@ -17,38 +18,38 @@ EUR = '\u20ac'
 
 # языки, что мы запрашиваем в качестве ключевых слов
 technologies = [
-    # '1С',
-    # 'Assembler',
-    # 'C',
-    # 'C++',
-    # 'C#',
-    # 'Clojure',
-    # 'Cuda',
-    # 'Delphi',
+    '1С',
+    'Assembler',
+    'C',
+    'C++',
+    'C#',
+    'Clojure',
+    'Cuda',
+    'Delphi',
     'Erlang',
-    # 'F#',
-    # 'Go',
-    # 'Groovy',
-    # 'Haskell',
-    # 'Java',
-    # 'JavaScript',
-    # 'Kotlin',
-    # 'Lisp',
-    # 'Lua',
-    # 'Matlab',
-    # 'Objective-C',
-    # 'OpenGL',
-    # 'Perl',
-    # 'PHP',
-    # 'Python',
-    # 'Ruby',
-    # 'Rust',
-    # 'Scala',
-    # 'Solidity',
-    # 'SQL',
-    # 'Swift',
-    # 'TypeScript',
-    # 'Verilog',
+    'F#',
+    'Go',
+    'Groovy',
+    'Haskell',
+    'Java',
+    'JavaScript',
+    'Kotlin',
+    'Lisp',
+    'Lua',
+    'Matlab',
+    'Objective-C',
+    'OpenGL',
+    'Perl',
+    'PHP',
+    'Python',
+    'Ruby',
+    'Rust',
+    'Scala',
+    'Solidity',
+    'SQL',
+    'Swift',
+    'TypeScript',
+    'Verilog',
 ]
 
 # данные для передачи в эксельку
@@ -133,62 +134,42 @@ def get_salary_and_vacancies(data_list: list) -> tuple:
     Returns:
         tuple: средняя макс. ЗП, средняя мин. ЗП, кол-во вакансий
     """
-    # общее сумма минимальных и максимальных ЗП
-    total_floor_salary = 0
-    total_ceiling_salary = 0
-    # число вакансий содержащих минимальные и максимальные ЗП
-    number_of_floor_salaries = 0
-    number_of_ceiling_salaries = 0
-    # возвращаемые значения:
-    #   средняя максимальная ЗП,
-    #   средняя минимальная ЗП,
-    #   общее число вакансий на рынке (не более 2000)
-    ret_ceiling_salary = 0
-    ret_floor_salary = 0
+    # список минимальных и максимальных ЗП
+    list_floor_salary = []
+    list_ceiling_salary = []
+    # общее число вакансий на рынке (не более 2000)
     ret_number_of_vacancies = 0
     # цикл, перебора по списку data_list, содержащий ответы на запросы к hh.ru
-    for ret_number_of_vacancies, vacancy in enumerate(data_list):
-        # считаем общее число вакансий ret_number_of_vacancies += 1
+    for vacancy in data_list:
+        # считаем общее число вакансий
+        ret_number_of_vacancies += 1
         # проверяем есть ли значения в словаре по ключу salary. Т.е проверяем есть ли в вакансии данные по зарплате
         if vacancy['salary'] is not None:
             # записываем значение в переменную salary
             salary = vacancy['salary']
             # проверяем есть ли значения по ключу from. Т.е проверяем есть ли в вакансии данные по минимальной зп
             if salary['from'] is not None:
-                # считаем количество обработанных вакансий в которых указана минимальная ЗП
-                number_of_floor_salaries += 1
                 # считаем сумму минимальной ЗП по вакансиям в зависимости от валюты
                 if salary['currency'] == 'USD':
-                    total_floor_salary += salary['from'] * USD_to_RUR
+                    list_floor_salary.append(salary['from'] * USD_to_RUR)
                 elif salary['currency'] == 'EUR':
-                    total_floor_salary += salary['from'] * EUR_to_RUR
+                    list_floor_salary.append(salary['from'] * EUR_to_RUR)
                 else:
-                    total_floor_salary += salary['from']
+                    list_floor_salary.append(salary['from'])
             # проверяем есть ли значения по ключу to. Т.е проверяем есть ли в вакансии данные по максимальной зп
             if salary['to'] is not None:
-                # считаем количество обработанных вакансий в которых указана минимальная и максимальная ЗП
-                number_of_ceiling_salaries += 1
                 # считаем сумму средней ЗП по вакансиям в зависимости от валюты
                 if salary['currency'] == 'USD':
-                    total_ceiling_salary += salary['to'] * USD_to_RUR
+                    list_ceiling_salary.append(salary['to'] * USD_to_RUR)
                 elif salary['currency'] == 'EUR':
-                    total_ceiling_salary += salary['to'] * EUR_to_RUR
+                    list_ceiling_salary.append(salary['to'] * EUR_to_RUR)
                 else:
-                    total_ceiling_salary += salary['to']
-    # считаем средние арифметические значения, если это возможно
-    try:
-        ret_ceiling_salary = total_ceiling_salary // number_of_ceiling_salaries
-    except ZeroDivisionError:
-        print('Number of the vacancies with maximal salary is null')
-    try:
-        ret_floor_salary = total_floor_salary // number_of_floor_salaries
-    except ZeroDivisionError:
-        print('Number of the vacancies with minimal salary is null')
-    return ret_ceiling_salary, ret_floor_salary, ret_number_of_vacancies
+                    list_ceiling_salary.append(salary['to'])
+    return int(median(list_ceiling_salary)), int(median(list_floor_salary)), ret_number_of_vacancies
 
 
 def get_data_for_excel(salary, number, keyword):
-    return
+    pass
 
 
 def save_data_to_json(pages, name) -> None:
@@ -253,11 +234,18 @@ for technology in technologies:
             technology, RUR, floor_salary, RUR, ceiling_salary,
         ))
     else:
-        print('a/an {:>11} developer has nothing to do'.format(technology))
+        print('a/an {0:>11} developer has nothing to do'.format(technology))
     # Необязательная задержка, чтобы не нагружать сервисы hh
     time.sleep(0.25)
 # получение текущей даты
 date = datetime.now()
 # сохранение данных в эксельку
 df = pandas.DataFrame(data_for_xls)
-df.to_excel('./{0}.{1}.{2}_data.xlsx'.format(date.day, date.month, date.year), sheet_name='data')
+df.to_excel(
+    './{0}.{1}.{2}_data.xlsx'.format(
+        date.day,
+        date.month,
+        date.year,
+    ),
+    sheet_name='data',
+)
